7db70ac83f06f76e6ede682228742d2f5b8125ee
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2024-03-29 10:32:14 +0100

    reformated the views.py file

--- core/views.py
+++ core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 import requests
 from .models import City
 from .forms import CityForm
-from datetime import datetime
+
 
 def weather_list(request):
     cities = City.objects.all()
@@ -21,26 +21,26 @@ def weather_list(request):
         data = get_current_weather(city.name, city.country)
         city_weather = {
             'name': data['name'],
-            'main' : data['weather'][0]['main'], 
-            'desc' : data['weather'][0]['main'],
-            'icon' : data['weather'][0]['icon'],
-            'main_temp' : data['main']['temp'],
-            'max_temp' : data['main']['temp_min'],
+            'main': data['weather'][0]['main'],
+            'desc': data['weather'][0]['main'],
+            'icon': data['weather'][0]['icon'],
+            'main_temp': data['main']['temp'],
+            'max_temp': data['main']['temp_min'],
             'min_temp': data['main']['temp_min'],
             'visiblity': data['visibility'],
-            'wind_speed' : data['wind']['speed'],
+            'wind_speed': data['wind']['speed'],
             'humidity': data['main']['humidity']
         }
         weather_data.append(city_weather)
 
-    
     context = {'weather_data': weather_data, 'form': form}
     return render(request, 'core/list.html', context=context)
 
 
 # Get the latitude of a city in a partuclar country 
 def _get_lat_lon(city, country):
-    url = f'http://api.openweathermap.org/geo/1.0/direct?q={city},{country}&limit=1&appid=98e201f7d727e4a86953ff2b72e265eb'
+    url = (f'https://api.openweathermap.org/geo/1.0/direct?q={city},{country}&limit=1&appid'
+           f'=98e201f7d727e4a86953ff2b72e265eb')
     r = requests.get(url)
     if r.status_code == 200:
         data = r.json()
@@ -55,7 +55,8 @@ def _get_lat_lon(city, country):
 def get_current_weather(city, country):
     if _get_lat_lon(city, country):
         lat, lon = _get_lat_lon(city, country)
-        url = f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=98e201f7d727e4a86953ff2b72e265eb&units=metric'
+        url = (f'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid'
+               f'=98e201f7d727e4a86953ff2b72e265eb&units=metric')
         response = requests.get(url)
         if response.status_code == 200:
             data = response.json()
@@ -63,15 +64,17 @@ def get_current_weather(city, country):
     else:
         print("Cant fetch data")
     
+
 def forecast(request):
-    url = f"https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=98e201f7d727e4a86953ff2b72e265eb&units=metric"
+    url = (f"https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid"
+           f"=98e201f7d727e4a86953ff2b72e265eb&units=metric")
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
         forecast_data = get_forecast_weather(data)
     else:
         forecast_data = None
-    context = {'weather_data':weather_data}
+    context = {'forecast_data': forecast_data}
     return render(request, 'core/forecast.html', context=context)
 
 
